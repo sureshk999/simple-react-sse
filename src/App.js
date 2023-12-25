bfacd53443ec4d603343ad3ff885648396428569
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-12-25 12:26:28 +1000

    change port to 3005

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    const eventSource = new EventSource('http://localhost:1000/events');
+    const eventSource = new EventSource('http://localhost:3500/events');
     eventSource.onmessage = (event) => {
       const message = event.data;
       setMessages((prevMessages) => [...prevMessages, message]);
